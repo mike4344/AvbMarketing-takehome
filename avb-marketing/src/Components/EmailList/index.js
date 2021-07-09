d589c1ab1579465d8ebabf63086269e348636de4
mike <mikemihalchik@gmail.com>
2021-07-09 15:46:23 -0400

    updated comments on what to add next when I begin work on this in the morning

--- avb-marketing/src/Components/EmailList/index.js
+++ avb-marketing/src/Components/EmailList/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function EmailList() {
+    const [emailList, setEmailList] = useState([])
+    // fetch all emails associated with the current contact Id
 
 	return (
 		<div className=''>
-
+            {/* each email will have an on hover function that will make a delete button show */}
 		</div>
 	);
 }
