e305eedd3203e7ef66cd9a9234ac94dbc64a2643
mike <mikemihalchik@gmail.com>
2021-07-10 10:46:42 -0400

    added the fetch request for the paginted list of contacts

--- README.md
+++
@@ -1,1 +0,0 @@
-# AvbMarketing-takehome
--- avb-marketing/src/Components/Contacts/index.js
+++ avb-marketing/src/Components/Contacts/index.js
@@ -3,10 +3,35 @@ import React, {useState, useEffect} from "react";
 export default function Contacts() {
     const [contactList, setContactList] = useState([])
     const [currentContact, setCurrentContact] = useState({})
+    const [currentPage, setCurrentPage] = useState(1)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
     // Fetch list of current contacts
+    useEffect(() =>{
+        fetch(`https://avb-contacts-api.herokuapp.com/contacts/paginated?page=${currentPage}&itemsPerPage=20`)
+        .then(response=>{
+            if (response.ok) return response.json()
+            throw response
+        })
+        .then(contacts => {
+            setContactList(contacts)
+        })
+        .catch(error =>{
+            throw new Error(`There was the following error: ${error}`)
+        })
+        .finally(() => setLoading(false))
+        return () => {
+            setLoading(true)
+            setError(null)
+        }
+    },[currentPage])
 	return (
 		<div className=''>
             {/* check if fetch request is returned if so map through the objects */}
+            {loading && 'Loading...'}
+            {!loading && contactList.map(contact =>{
+                
+            })}
             {/* create contact components and pass through the contacts info to the component */}
             {/* Onclick setting currentContact */}
 		</div>
